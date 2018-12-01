2ec6a36c84d4b7bf203448096c0465ed57371b82
Timo Reitz <godsboss@gmx.de>
2018-12-01 04:57:29 +0100

    Add Loop class.

---
+++ src/js/Loop.js
@@ -0,0 +1,40 @@
+class Loop {
+  constructor(repeat, action) {
+    this.repeat = repeat
+    this.action = action
+    this.running = false
+  }
+
+  start () {
+    if (this.running) {
+      return false
+    }
+    this.step()
+    return true
+  }
+
+  step () {
+    if (!this.running) {
+      return false
+    }
+    this.repeat(
+      () => this.step()
+    )
+    this.action()
+    return true
+  }
+
+  stop() {
+    const stopped = this.running
+    this.running = false
+    return stopped
+  }
+}
+
+function start(repeat, action) {
+  const loop = new Loop(repeat, action)
+  loop.start()
+  return loop
+}
+
+export { start, Loop }
