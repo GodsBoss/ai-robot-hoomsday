716912c32d7259e0b944e6f7f6c941d2e003cdc0
Timo Reitz <godsboss@gmx.de>
2018-12-01 09:11:04 +0100

    Add state handling to Game.

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,24 @@
+import State from './State'
+
 export default class Game{
   constructor(){
+    // Contains game-relevant data. Survives state switches.
+    this.data = {}
+
+    // Contains objects to be rendered. Cleared on state switch.
+    this.objects = []
+
+    // Initially, a NOP state.
+    this.state = new State()
   }
 
-  tick() {}
+  nextState(state) {
+    this.state = state
+    this.objects = []
+    state.init(this)
+  }
+
+  tick() {
+    this.state.tick(this)
+  }
 }
