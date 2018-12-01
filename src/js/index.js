5031175f49bd3fe8269a8b9beb2da0d1e3cdfc74
Timo Reitz <godsboss@gmx.de>
2018-12-01 07:29:11 +0100

    Load gfx on window.load.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
+import loader from './loader'
+import wait from './wait'
+
 window.addEventListener('load', init, false)
 
-function init(e) {}
+function init(e) {
+  const image = loader.image("gfx.png")
+  wait(
+    [
+      image.load()
+    ],
+    () => {}
+  )
+}
