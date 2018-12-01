bce70b4451e5b8122b9d7f3ab9d951751a0e7b80
Timo Reitz <godsboss@gmx.de>
2018-12-01 20:38:21 +0100

    Extract level initialization into function.

--- src/js/states/Play.js
+++ src/js/states/Play.js
@@ -8,28 +8,7 @@ export default class Play extends State {
   }
 
   init(game) {
-    for(let column = 0; column < 18; column++) {
-      for(let row = 0; row < 12; row++) {
-        game.objects.push(
-          {
-            type: 'tile_ground',
-            x: column * 16 + gridOffset.x,
-            y: row * 16 + gridOffset.y
-          }
-        )
-      }
-    }
-    this.levels[game.data.levelToPlay].tiles.forEach(
-      (tile) => game.objects.push(
-        {
-          type: tile.type,
-          x: tile.col * 16 + gridOffset.x,
-          y: tile.row * 16 + gridOffset.y,
-          w: 16,
-          h: 16
-        }
-      )
-    )
+    initLevel(game, this.levels[game.data.levelToPlay])
   }
 
   tick(game) {}
@@ -46,6 +25,32 @@ export default class Play extends State {
       }
     }
   }
+}
+
+function initLevel(game, level) {
+  game.objects = []
+  for(let column = 0; column < 18; column++) {
+    for(let row = 0; row < 12; row++) {
+      game.objects.push(
+        {
+          type: 'tile_ground',
+          x: column * 16 + gridOffset.x,
+          y: row * 16 + gridOffset.y
+        }
+      )
+    }
+  }
+  level.tiles.forEach(
+    (tile) => game.objects.push(
+      {
+        type: tile.type,
+        x: tile.col * 16 + gridOffset.x,
+        y: tile.row * 16 + gridOffset.y,
+        w: 16,
+        h: 16
+      }
+    )
+  )
 }
 
 const clickActions = {
