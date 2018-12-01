35aefebd7dde1c7ab8e71e2da260cea2848674ce
Timo Reitz <godsboss@gmx.de>
2018-12-01 07:51:34 +0100

    Run NOP game.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+import Game from './Game'
 import loader from './loader'
+import { start } from './Loop'
 import wait from './wait'
 
 window.addEventListener('load', init, false)
+
+// TPS is the amount of game ticks per second.
+const TPS = 25
 
 function init(e) {
   const image = loader.image("gfx.png")
@@ -9,6 +14,14 @@ function init(e) {
     [
       image.load()
     ],
-    () => {}
+    () => {
+      const game = new Game()
+      start(
+        (next) => {
+          window.setTimeout(next, 1000 / TPS, next)
+        },
+        () => game.tick()
+      )
+    }
   )
 }
