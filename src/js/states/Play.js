14481ca3e247010f540add582888da6f89ba0dc7
Timo Reitz <godsboss@gmx.de>
2018-12-01 20:03:03 +0100

    Play state renders grid and tiles (from level).

--- src/js/states/Play.js
+++ src/js/states/Play.js
@@ -6,9 +6,37 @@ export default class Play extends State {
     this.levels = levels
   }
 
-  init(game) {}
+  init(game) {
+    for(let column = 0; column < 18; column++) {
+      for(let row = 0; row < 12; row++) {
+        game.objects.push(
+          {
+            type: 'tile_ground',
+            x: column * 16 + gridOffset.x,
+            y: row * 16 + gridOffset.y
+          }
+        )
+      }
+    }
+    this.levels[game.data.levelToPlay].tiles.forEach(
+      (tile) => game.objects.push(
+        {
+          type: tile.type,
+          x: tile.col * 16 + gridOffset.x,
+          y: tile.row * 16 + gridOffset.y,
+          w: 16,
+          h: 16
+        }
+      )
+    )
+  }
 
   tick(game) {}
 
   invoke(game, event) {}
+}
+
+const gridOffset = {
+  x: 16,
+  y: 4
 }
