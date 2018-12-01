4cff3749a08909ab11760b4aa0802b45d303d2ad
Timo Reitz <godsboss@gmx.de>
2018-12-01 21:18:42 +0100

    Create layout for first level.

--- src/js/levels.js
+++ src/js/levels.js
@@ -8,18 +8,51 @@ const levels = [
     tiles: [
       {
         type: "tile_exit",
-        col: 2,
-        row: 2
+        col: 14,
+        row: 1
       },
       {
         type: "tile_reset",
-        col: 3,
+        col: 14,
         row: 2
       },
       {
         type: "tile_start_stop",
+        col: 14,
+        row: 3
+      },
+      {
+        type: "tile_source",
+        col: 2,
+        row: 8,
+        amount: 5,
+        direction: "right"
+      },
+      {
+        type: "tile_block",
+        col: 5,
+        row: 8
+      },
+      {
+        type: "tile_block",
         col: 4,
-        row: 2
+        row: 10
+      },
+      {
+        type: "tile_block",
+        col: 2,
+        row: 9
+      },
+      {
+        type: "tile_block",
+        col: 3,
+        row: 4
+      },
+      {
+        type: "tile_sink",
+        col: 12,
+        row: 5,
+        amount: 5
       }
     ]
   },
