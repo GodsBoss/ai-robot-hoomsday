e50d66c1cceac366436d0021b6eab18e0b73415c
Timo Reitz <godsboss@gmx.de>
2018-12-01 19:46:59 +0100

    Prepare Play state for more work.

--- src/js/states/Play.js
+++ src/js/states/Play.js
@@ -1,3 +1,14 @@
 import State from '../State'
 
-export default class Play extends State {}
+export default class Play extends State {
+  constructor(levels) {
+    super()
+    this.levels = levels
+  }
+
+  init(game) {}
+
+  tick(game) {}
+
+  invoke(game, event) {}
+}
