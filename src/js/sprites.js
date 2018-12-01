92d68cccd7907de09aa3b583d8f44f87b4dd62ad
Timo Reitz <godsboss@gmx.de>
2018-12-01 14:12:37 +0100

    Add character set.

--- src/js/sprites.js
+++ src/js/sprites.js
@@ -1,11 +1,53 @@
-const sprites = [
-  {
-    type: "bg_title",
-    x: 320,
-    y: 0,
-    w: 320,
-    h: 200
+const sprites = [].concat(
+  [
+    {
+      type: "bg_title",
+      x: 320,
+      y: 0,
+      w: 320,
+      h: 200
+    }
+  ],
+  createLetterSprites()
+);
+
+function createLetterSprites() {
+  const sprites = []
+  for(let charCode = 65; charCode <= 90; charCode++) {
+    sprites.push(
+      {
+        type: `letter_${String.fromCharCode(charCode)}`,
+        x: (charCode - 65) * 6,
+        y: 0,
+        w: 5,
+        h: 5
+      }
+    )
   }
-]
+  for(let i = 0; i <= 9; i++) {
+    sprites.push(
+      {
+        type: `letter_${i}`,
+        x: (26 + i) * 6,
+        y: 0,
+        w: 5,
+        h: 5
+      }
+    )
+  }
+  const punctuation = ['.', ',', '!', '?', ':', '-', '=']
+  punctuation.forEach(
+    (char, index) => sprites.push(
+      {
+        type: `letter_${char}`,
+        x: (26 + 10 + index) * 6,
+        y: 0,
+        w: 5,
+        h: 5
+      }
+    )
+  )
+  return sprites
+}
 
 export default sprites
